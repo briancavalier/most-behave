aa33b28010aa99d970c8d3e0ec92d5e10961d8bc
Brian Cavalier <brian@hovercraftstudios.com>
2017-01-17 08:06:54 -0500

    feat(behave): Add snapshot
    AFFECTS: @most/behave
    
    Add snapshot, simpilify build, add flow types, remove dist/ from repo

---
+++ src/event.js
@@ -0,0 +1,26 @@
+// ------------------------------------------------------
+// Event helpers
+
+export const mapWithTimeE = (f, stream) => new stream.constructor({
+  run: (sink, scheduler) => stream.source.run(new MapWithTimeSink(f, sink), scheduler)
+})
+
+class MapWithTimeSink {
+  constructor (f, sink) {
+    this.f = f
+    this.sink = sink
+  }
+
+  event (t, x) {
+    const f = this.f
+    this.sink.event(t, f(t, x))
+  }
+
+  error (t, e) {
+    this.sink.error(t, e)
+  }
+
+  end (t, x) {
+    this.sink.end(t, x)
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,78 +1,119 @@
-import { zip, constant, map as mapE, startWith, sampleWith, timestamp } from 'most'
+import { zip, constant, map as mapE, startWith, sample as sampleE } from '@most/core'
 import multicast from '@most/multicast'
 import { curry2, curry3 } from '@most/prelude'
+import { mapWithTimeE } from './event'
 
 // Possibly useful:
-// 1. snapshot :: (a -> b -> c) -> Event a -> Behavior b -> Event c
-// 2. accum :: a -> Event (a -> a) -> Behavior a
+// - accum :: a -> Event (a -> a) -> Behavior a
 //    accum :: (a -> b -> c) -> a -> Event b -> Behavior c
-// 3. count :: Event a -> Behavior number
-// 4. when :: Behavior bool -> Event a -> Event a
+// - count :: Event a -> Behavior number
+// - when :: Behavior bool -> Event a -> Event a
 
 export const sample = curry2((event, behavior) => behavior.sample(event))
 
+export const snapshot = curry3((f, event, behavior) => behavior.snapshot(f, event))
+
+// Base Behavior typeclass
+// Implementations MUST define at least one of sample or snapshot, and
+// MAY define both as an optimization
+class Behavior {
+  sample (event) {
+    return this.snapshot(snd, event)
+  }
+
+  snapshot (f, event) {
+    const me = multicast(event)
+    return sampleE(f, this.sample(me), me)
+  }
+}
+
 export const always = x => new Constant(x)
 
-class Constant {
+class Constant extends Behavior {
   constructor (value) {
+    super()
     this.value = value
   }
 
-  sample (stream) {
-    return constant(this.value, stream)
+  sample (event) {
+    return constant(this.value, event)
+  }
+
+  snapshot (f, event) {
+    return mapE(a => f(a, this.value), event)
   }
 }
 
-class Computed {
+class Computed extends Behavior {
   constructor (f) {
+    super()
     this.f = f
   }
 
-  sample (stream) {
-    return mapE(this.f, timestamp(stream))
+  sample (event) {
+    return mapWithTimeE(this.f, event)
+  }
+
+  snapshot (g, event) {
+    const f = this.f
+    return mapWithTimeE((t, a) => g(a, f(t, a)), event)
   }
 }
 
-const getTime = ({ time }) => time
+const getTime = (t, x) => t
 export const time = new Computed(getTime)
 
-export const stepper = curry2((initial, updates) =>
-  new Stepper(startWith(initial, updates)))
+export const stepper = curry2((initial, updateEvent) =>
+  new Stepper(startWith(initial, updateEvent)))
 
-class Stepper {
+const snd = (a, b) => b
+
+class Stepper extends Behavior {
   constructor (updates) {
+    super()
     this.updates = updates
   }
 
-  sample (stream) {
-    return sampleWith(stream, this.updates)
+  sample (event) {
+    return sampleE(snd, event, this.updates)
+  }
+
+  snapshot (f, event) {
+    return sampleE(f, event, this.updates)
   }
 }
 
 export const map = curry2((f, behavior) => new Map(f, behavior))
 
-class Map {
+class Map extends Behavior {
   constructor (f, behavior) {
+    super()
     this.f = f
     this.behavior = behavior
   }
 
-  sample (stream) {
-    return mapE(this.f, this.behavior.sample(stream))
+  sample (event) {
+    return mapE(this.f, this.behavior.sample(event))
+  }
+
+  snapshot (g, event) {
+    const f = this.f
+    return this.behavior.snapshot((a, b) => g(a, f(b)), event)
   }
 }
 
 export const liftA2 = curry3((f, b1, b2) => new LiftA2(f, b1, b2))
 
-class LiftA2 {
+class LiftA2 extends Behavior {
   constructor (f, b1, b2) {
+    super()
     this.f = f
     this.b1 = b1
     this.b2 = b2
   }
 
-  sample (stream) {
-    const s = multicast(stream)
+  sample (event) {
+    const s = multicast(event)
     return zip(this.f, this.b1.sample(s), this.b2.sample(s))
   }
 }
